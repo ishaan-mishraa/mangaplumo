87baacb85ec6f9f6c0ebe5c13081ac4a22f97d0d
Ishaan Mishra <ishaancodes01@gmail.com>
2025-05-01 16:03:41 +0530

    fixed naming error

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mangaRouter = require('./MangaRouter');
+const mangaRouter = require('./mangaRouter');
 
 const app = express();
 
