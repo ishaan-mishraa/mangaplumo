05d170ec3ccd83859e91d3d74121320d0c7325e0
Ishaan Mishra <ishaancodes01@gmail.com>
2025-05-01 19:35:53 +0530

    updated css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
       <header className="bg-gray-800 py-4 px-6 flex items-center justify-between">
-        <h1 className="text-2xl font-bold">MangaPlumo</h1>
+        <h1 className="text-2xl font-bold agu-display">MangaPlumo</h1>
         <div className="space-x-4">
           <span className="material-icons align-middle">search</span>
           <span className="material-icons align-middle">notifications</span>
